1f84dfcd67a2437be9320e86aee6e875ca26b1b3
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-19 15:39:39 -0400

    configure 'player stats' migration table

---
+++ data/migrations/20200719153329_playerStats.js
@@ -0,0 +1,17 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("player stats", table => {
+    table.increments() // creates an auto incrementing field named 'id'
+    table.integer('batting average').notNullable
+    table.integer('hit').notNullable()
+    table.integer('at bat').notNullable()
+    table.integer('RBI').notNullable()
+    table.integer('single').notNullable()
+    table.integer('double').notNullable()
+    table.integer('triple').notNullable()
+    table.integer('homerun').notNullable()
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('player stats')
+};

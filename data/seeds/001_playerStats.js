b0659b69ce1f785d5ba5b5733e88998cc34bc2ff
Cobrettie <cobrettiegarner3@gmail.com>
2020-07-19 15:49:12 -0400

    configure 001 seed for player_stats

--- data/seeds/001_playerStats.js
+++ data/seeds/001_playerStats.js
@@ -4,10 +4,29 @@ exports.seed = function(knex) {
   return knex('player_stats').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('player_stats').insert([
+        {
+          id: 1,
+          batting_average: 0.500,
+          hit: 475,
+          at_bat: 950,
+          RBI: 148,
+          single: 35,
+          double: 80,
+          triple: 113,
+          homerun: 247
+        },
+        {
+          id: 2,
+          batting_average: 0.300,
+          hit: 300,
+          at_bat: 900,
+          RBI: 97,
+          single: 35,
+          double: 183,
+          triple: 18,
+          homerun: 64
+        }
       ]);
     });
 };
